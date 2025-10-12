0b3bee3022d1afc56a276c2b67ff31752e0b830b
feliperasilva <fras2@discente.ifpe.edu.br>
2025-10-11 23:52:08 -0300

    feat: crating module for donor

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DonorService } from './donor/donor.service';
 import { DonorController } from './donor/donor.controller';
+import { DonorModule } from './donor/donor.module';
 
 @Module({
-  imports: [],
+  imports: [DonorModule],
   controllers: [AppController, DonorController],
   providers: [AppService, DonorService],
 })
