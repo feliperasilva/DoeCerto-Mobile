2e3c52c2d02666eb7a565087edb83857f8485056
feliperasilva <fras2@discente.ifpe.edu.br>
2025-10-11 23:21:19 -0300

    feat: configurating connection in donor service

--- backend/src/donor/donor.service.ts
+++ backend/src/donor/donor.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { DonorDto } from './dto/donor.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class DonorService {
+    constructor(private prisma: PrismaService) {}
+
     create(data: DonorDto) {
 
     }
