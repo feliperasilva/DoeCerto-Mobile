37f8f76bad809896d9c5e2a45bb81a98d34bfe48
feliperasilva <fras2@discente.ifpe.edu.br>
2025-10-13 15:45:48 -0300

    feat: creating method Get for Donors

--- backend/src/donor/donor.controller.ts
+++ backend/src/donor/donor.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import type { DonorDto } from './dto/donor.dto';
 import { DonorService } from './donor.service';
 
@@ -7,8 +7,12 @@ export class DonorController {
 
     constructor (private readonly donorService: DonorService) {}
     @Post()
-
     async create(@Body() data: DonorDto) {
         return this.donorService.create(data);
+    }
+
+    @Get()
+    async findAll() {
+        return this.donorService.findAll();
     }
 }
