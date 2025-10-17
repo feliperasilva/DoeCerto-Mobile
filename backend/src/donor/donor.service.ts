30b1786fb7bcb92bcca51c1152115e8d7adde9db
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-10-17 16:43:30 -0300

    fix: format donor service

--- backend/src/donor/donor.service.ts
+++ backend/src/donor/donor.service.ts
@@ -4,68 +4,68 @@ import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class DonorService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async create(data: DonorDto) {
-        const donor = await this.prisma.donor.create({
-            data
-        });
+  async create(data: DonorDto) {
+    const donor = await this.prisma.donor.create({
+      data,
+    });
 
-        return donor;
+    return donor;
+  }
+
+  async findAll() {
+    return await this.prisma.donor.findMany();
+  }
+
+  async update(id: number, data: DonorDto) {
+    const donorExists = await this.prisma.donor.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!donorExists) {
+      throw new Error('Doador não encontrado');
     }
 
-    async findAll() {
-        return await this.prisma.donor.findMany();
+    return await this.prisma.donor.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+
+  async delete(id: number) {
+    const donorExists = await this.prisma.donor.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!donorExists) {
+      throw new Error('Doador não encontrado');
     }
 
-    async update(id: number, data: DonorDto) {
-        const donorExists = await this.prisma.donor.findUnique({
-            where: {
-                id,
-            }
-        });
+    return await this.prisma.donor.delete({
+      where: {
+        id,
+      },
+    });
+  }
 
-        if (!donorExists) {
-            throw new Error('Doador não encontrado')
-        }
+  async getById(id: number) {
+    const donorExists = await this.prisma.donor.findUnique({
+      where: {
+        id,
+      },
+    });
 
-        return await this.prisma.donor.update({
-            data,
-            where: {
-                id,
-            }
-        });
+    if (!donorExists) {
+      throw new Error('Doador não encontrado');
     }
 
-    async delete(id: number) {
-        const donorExists = await this.prisma.donor.findUnique({
-            where: {
-                id,
-            }
-        });
-
-        if (!donorExists) {
-            throw new Error('Doador não encontrado')
-        }
-
-        return await this.prisma.donor.delete({
-            where: {
-                id,
-            }
-        });
-    }
-
-    async getById(id: number) {
-        const donorExists = await this.prisma.donor.findUnique({
-            where: {
-                id,
-            }
-        });
-
-        if (!donorExists) {
-            throw new Error('Doador não encontrado')
-        }
-
-        return donorExists;
-    }
+    return donorExists;
+  }
 }
