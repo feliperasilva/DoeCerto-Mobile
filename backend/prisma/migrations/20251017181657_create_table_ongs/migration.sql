47e89f68640ca5df0dbe6b3f31f91128e8a7d887
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-10-17 16:42:24 -0300

    feat: create ongs table

---
+++ backend/prisma/migrations/20251017181657_create_table_ongs/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `ongs` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `cnpj` VARCHAR(18) NOT NULL,
+
+    UNIQUE INDEX `ongs_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
