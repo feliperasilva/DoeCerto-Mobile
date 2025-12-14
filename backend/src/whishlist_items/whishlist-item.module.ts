47dd1926fc0c79529275e95318e825bd049d152e
feliperasilva <fras2@discente.ifpe.edu.br>
2025-12-14 15:22:57 -0300

    feat: Create module for wishlist

---
+++ backend/src/whishlist_items/whishlist-item.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { WhishlistItemService } from './whishlist-item.service';
+import { WishlistItemController } from './wishlist-item.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [WishlistItemController],
+  providers: [WhishlistItemService],
+  exports: [WhishlistItemService],
+})
+export class WhishlistItemModule {}
